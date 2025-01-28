a59483c69576e8def88146234a5ad819ec6279a0
Joe Knowles <joejknowles@gmail.com>
2025-01-28 10:43:48 +0000

    Improve experience title wrapping on smaller screens

--- src/components/cv/Experience.tsx
+++ src/components/cv/Experience.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 interface ExperienceProps {
     children: React.ReactNode;
 }
@@ -38,19 +40,6 @@ Experience.VideoSection = ({ src }: { src: string }) => {
     );
 }
 
-Experience.Title = ({ children, href }: { children: React.ReactNode, href?: string }) => {
-    return (
-        <a
-            href={href}
-            className="text-xl text-blue-700 font-semibold mr-2 hover:text-blue-500"
-            target="_blank"
-            rel="noreferrer"
-        >
-            {children}
-        </a>
-    );
-}
-
 Experience.DescriptionParagraph = ({
     children,
     ...rest
@@ -70,27 +59,50 @@ Experience.ToolsList = ({ tools }: { tools: string[] }) => {
 }
 
 Experience.HeadingSection = ({ children }: { children: React.ReactNode }) => {
+    const childrenArray = React.Children.toArray(children);
+    const childrenWithSpacing = childrenArray.reduce((acc: React.ReactNode[], child, index) => {
+        if (index === 0) {
+            return [child];
+        }
+        return [...acc, " ", child];
+    }, [] as React.ReactNode[]);
     return (
         <div className="text-xl">
-            {children}
+            {childrenWithSpacing}
         </div>
+    );
+}
+
+
+Experience.Title = ({ children, href }: { children: React.ReactNode, href?: string }) => {
+    return (
+        <a
+            href={href}
+            className="text-xl text-blue-700 font-semibold hover:text-blue-500"
+            target="_blank"
+            rel="noreferrer"
+        >
+            {children}
+        </a>
     );
 }
 
 Experience.Location = ({ children }: { children: React.ReactNode }) => {
     return (
-        <span className="text-gray-500 font-semibold mr-2">– {children}</span>
+        <span className="text-gray-500 font-semibold whitespace-nowrap">
+            <span className="hidden xs:inline">– </span>{children}
+        </span>
     );
 }
 
 Experience.Date = ({ children }: { children: React.ReactNode }) => {
     return (
-        <span className="text-gray-500 mr-2">{children}</span>
+        <span className="text-gray-500 whitespace-nowrap">{children}</span>
     );
 }
 
 Experience.JobTitle = ({ children }: { children: React.ReactNode }) => {
     return (
-        <span className="text-gray-500 italic mr-2">{children}</span>
+        <span className="text-gray-500 italic whitespace-nowrap">{children}</span>
     );
 }
