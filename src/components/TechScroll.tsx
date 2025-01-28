323adb71cf0548429734ebe8cab4f5e92b8b2745
Joe Knowles <joejknowles@gmail.com>
2025-01-28 15:41:31 +0000

    Add other tech

--- src/components/TechScroll.tsx
+++ src/components/TechScroll.tsx
@@ -6,10 +6,36 @@ const technologies = [
     { name: "Typescript", logo: "/logos/typescript.png" },
     { name: "GPT API", logo: "/logos/gpt.png" },
     { name: "Prompt engineering", logo: "/logos/prompt.png" },
-    // Add more technologies and their logos here
+    { name: "Ionic", logo: "/logos/ionic.png" },
+    { name: "threeJS", logo: "/logos/threejs.png" },
+    { name: "react-three-fiber", logo: "/logos/react-three-fiber.png" },
+    { name: "Capacitor", logo: "/logos/capacitor.png" },
+    { name: "Android Studio", logo: "/logos/android-studio.png" },
+    { name: "Firestore", logo: "/logos/firestore.png" },
+    { name: "Cypress", logo: "/logos/cypress.png" },
+    { name: "Firebase Auth", logo: "/logos/firebase-auth.png" },
+    { name: "Ruby on Rails", logo: "/logos/ruby-on-rails.png" },
+    { name: "PostgresQL", logo: "/logos/postgresql.png" },
+    { name: "Styled components", logo: "/logos/styled-components.png" },
+    { name: "Render", logo: "/logos/render.png" },
+    { name: "Expo", logo: "/logos/expo.png" },
+    { name: "NextJS", logo: "/logos/nextjs.png" },
+    { name: "AWS", logo: "/logos/aws.png" },
+    { name: "EC2", logo: "/logos/ec2.png" },
+    { name: "Material-ui", logo: "/logos/material-ui.png" },
+    { name: "Prisma", logo: "/logos/prisma.png" },
+    { name: "GraphQL", logo: "/logos/graphql.png" },
+    { name: "Apollo", logo: "/logos/apollo.png" },
+    { name: "RDS", logo: "/logos/rds.png" },
+    { name: "redux", logo: "/logos/redux.png" },
+    { name: "Tailwind CSS", logo: "/logos/tailwind-css.png" },
+    { name: "Design Tokens", logo: "/logos/design-tokens.png" },
+    { name: "React Native", logo: "/logos/react-native.png" },
+    { name: "dotnet", logo: "/logos/dotnet.png" },
+    { name: "SASS", logo: "/logos/sass.png" },
 ];
 
-const technologiesDoubled = [...technologies, ...technologies, ...technologies, ...technologies];
+const technologiesDoubled = [...technologies, ...technologies];
 
 const numberOfTechs = technologiesDoubled.length;
 
